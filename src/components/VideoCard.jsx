ce4dd3f754dbac19cb24c38b4a434423403017ca
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-06-19 22:29:05 +0900

    feat Data Fetching

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
+import { formatAgo } from "../util/date";
 
 export default function VideoCard({ video }) {
-    return (
+  const { title, thumbnails, channelTitle, publishedAt } = video.snippet;
+  return (
+    <>
+      <li>
+        <img className='w-full' src={thumbnails.medium.url} alt={title} />
         <div>
-            {video.snippet.title}
+            <p className='font-semibold my-2 line-clamp-2'>{title}</p>
+            <p className='text-sm opacity-80'>{channelTitle}</p>
+            <p className='text-sm opacity-80'>{formatAgo(publishedAt, 'ko')}</p>
         </div>
-    );
+      </li>
+    </>
+  );
 }
-
