3e0be41fbd0b0132f6d841f2892def090264a7e2
cheolung12 <109952479+cheolung12@users.noreply.github.com>
2023-06-22 21:08:01 +0900

    add channels/related api

--- src/api/fakeYoutubeClient.js
+++ src/api/fakeYoutubeClient.js
@@ -3,11 +3,18 @@ import axios from 'axios';
 export default class FakeYoutubeClient {
 
     // 클래스 내부에서는 function 키워드 안 적어도 됨
-    async search() {
-        return axios.get('/videos/search.json');
+    async search(params) {
+        
+        return params.relatedToVideoId 
+        ? axios.get('/videos/related.json')
+        : axios.get('/videos/search.json')
     }
 
     async videos() {
         return axios.get('/videos/popular.json');
+    }
+
+    async channels() {
+        return axios.get('/videos/channel.json');
     }
 }
